285f10e737277e0c7e53736a91c84b0fe3747de8
Tudor A. Lascu <lascu@cs.unibo.it>
2013-10-07 17:05:17 +0200

    changed a couple of scripts in ./demo directory

--- demo/testB_100.sh
+++ demo/testB_100.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-../amin_planner.native -u ../input/testB_100_duplication.json -c C99 -s s2 -o ../results/testB_100_duplication_results.txt
+../_build/amin_planner.native -u ../input/testB_100_duplication.json -c C99 -s s2 -o ../results/testB_100_duplication_results.txt
