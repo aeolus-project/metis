ff738e29300ccaf47b7286fb3b16e9ce60eda9d4
Tudor A. Lascu <lascu@cs.unibo.it>
2014-01-22 11:31:07 +0100

    modified a few scripts to work with the new invocation syntax

--- demo/mysql_replication_demo.sh
+++ demo/mysql_replication_demo.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-../metis.native -u ../input/mysql_master_slave_replication.json -c Application -s Inst -o ../results/mysql_master_slave_replication_results.txt
+../metis.native -u ../input/mysql_master_slave_replication.json -c Application -s Inst -o ../results/mysql_master_slave_replication_results.txt -ap ../results/mysql_master_slave_replication_abstract_plan.dot 
