480bffd6d9cc601913f0c2d1aa28ff8613c665c7
Tudor A. Lascu <lascu@cs.unibo.it>
2014-02-07 09:28:27 +0100

    fixed script testB_5.sh for abstract plan generation

--- demo/testB_5.sh
+++ demo/testB_5.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-../metis.native -u ../input/testB_5.json -c C4 -s s2 -o ../results/testB_5_results.txt
+../metis.native -u ../input/testB_5.json -c C4 -s s2 -o ../results/testB_5_results.txt -ap ../results/testB_5_abstract_plan.dot
